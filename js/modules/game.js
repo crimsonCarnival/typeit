37e1e1d23e77b91b259aae75565e7eb3cec0b998
= <agalvis@cotecnova.edu.co>
2025-10-25 16:44:06 -0500

    Fix asset paths for GitHub Pages deployment

--- js/modules/game.js
+++ js/modules/game.js
@@ -5,9 +5,9 @@ import { generateWordContent } from '../words.js'
 
 let isMuted = false
 
-const keySound = new Audio('/assets/typewriter-single-key.mp3')
-const timeUpSound = new Audio('/assets/typewriter-bell.mp3')
-const errorSound = new Audio('/assets/error-sound.mp3')
+const keySound = new Audio('./assets/typewriter-single-key.mp3')
+const timeUpSound = new Audio('./assets/typewriter-bell.mp3')
+const errorSound = new Audio('./assets/error-sound.mp3')
 
 export const setMuted = muted => isMuted = muted
 
